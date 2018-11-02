86ecbd8922dec836999d797f0ae2b0980acb93ce
wanghuan <seuwh2018@foxmail.com>
2018-11-02 14:09:41 +0800

    add delay time to simulate write time-consuming

--- 算法和数据结构基础/3.1 暴力美学-排序/code/java/SelectionSort.java
+++ 算法和数据结构基础/3.1 暴力美学-排序/code/java/SelectionSort.java
@@ -1,28 +1,40 @@
+//SelectionSort.java
 import java.util.ArrayList;
 public class SelectionSort extends SortAlgorithm
 {
-	
-	/**
-	 * Sort array.
-	 * 
-	 * @param array Array to be sorted.
-	 */
     public void sort(int[] array) {
-        // IMPLEMENT ME
-		for (int i = 0; i < array.length - 1; ++i)
-		{
-			int min = i;
-			for (int j = i + 1; j < array.length; ++j )
-			{
-				if (array[j] < array[min])
-					min = j;
-			}
-			int temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-		}
+        for (int i = 0; i < array.length - 1; ++i)
+        {
+            int min = i;
+            for (int j = i + 1; j < array.length; ++j )
+            {
+                if (array[j] < array[min])
+                    min = j;
+            }
 
-    } // end of sort()
-
-
-} // end of class SelectionSort
+            new Thread(new Runnable() {
+                public void run() {
+                    while (true) {
+                        test();
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                private void test() {
+                    // TODO Auto-generated method stub
+                }
+                public Runnable start() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+            }.start());
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+} 
